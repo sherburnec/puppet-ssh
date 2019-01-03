b3f53d91a9c96d8626183b894fa0ed007c9bf899
root <root@sherburnechuck1c.mylabserver.com>
2019-01-03 20:37:00 +0000

    Replaced Params with functions

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,11 +43,14 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 #
 class ssh(
-  Boolean $permit_root_login = $::ssh::params::permit_root_login,
-  Integer $port              = $::ssh::params::port,
-  String $package_name       = $::ssh::params::package_name,
-  String $service_name       = $::ssh::params::service_name,
-) inherits ::ssh::params {
+  String $package_name,
+  String $service_name,
+  String $service_ensure,
+  String $ensure,
+  Integer $port,
+  Boolean $permit_root_login,
+  Boolean $service_enable,
+) {
   class { '::ssh::service': }
   class { '::ssh::config': }
   class { '::ssh::install': }
